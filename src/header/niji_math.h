fcf0d0e18b550196c70b461e923c453e987acc19
NijikawaSatoki <nijikawasatoki00@outlook.com>
2024-09-17 20:16:24 -0400

    Created header 'niji_math'

---
+++ src/header/niji_math.h
@@ -0,0 +1,17 @@
+#pragma once
+class NijiMath {
+    public:
+        void Niji_addition();
+        void Niji_subtraction();
+        void Niji_multiplication();
+        void Niji_division();
+        void Niji_modulus();
+        void Niji_exponent();
+        void Niji_cosine();
+        void Niji_sine();
+        void Niji_tangent();
+        void Niji_squareRoot();
+        void Niji_cubeRoot();
+        void Niji_square();
+        void Niji_cube();
+}
