ba616695eb6e6973da2975965964da3531e59523
NijikawaSatoki <nijikawasatoki00@outlook.com>
2024-10-19 01:19:46 -0400

    Added comments showing who wrote which function

--- src/class/NijiMisc.cpp
+++ src/class/NijiMisc.cpp
@@ -24,22 +24,30 @@
 // Establishes usage of `std' namespace
 using namespace std;
 
+/* Credits And Contributors
+ * Function author: Satoki Nijikawa (Niji System) */
 void NijiMisc::credits() {
     std::string nextLine = "\n";
     std::string singleTab = "\t";
     std::cout << "Niji's Many Function Calculator developed by:" << nextLine << "Satoki Nijikawa" << singleTab << "Wamo" << std::endl;
 }
 
+/* Exit
+ * Function author: Wamo (Niji System) */
 void NijiMisc::exit() {
     std::cout << "Oh. I see..." << std::endl;
 }
 
+/* No input
+ * Function author: Wamo (Niji System) */
 void NijiMisc::emptyInput() {
     std::cout << "You didn't say what operation you wanted. Like, you do know that I'm *not* a mind reader, right?" << std::endl;
     std::cout << "*sigh*" << std::endl;
     std::cout << "Maybe try saying what input you want next time." << std::endl;
 }
 
+/* Nonsensical input
+ * Function author: Wamo (Niji System) */
 void NijiMisc::unknownInput() {
     std::cout << "That's not a valid operation!!" << std::endl;
     std::cout << "Try again!!" << std::endl;
